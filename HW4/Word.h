a0c0638990efb48dfb8eb92291f0e6aa424ff878
James <jamesqian1999@gmail.com>
2020-11-26 09:24:15 +0800

    1125_1100

--- HW4/Word.h
+++ HW4/Word.h
@@ -7,26 +7,25 @@ class Word : public Token
 {
 public:
     string lexeme= "";
-    Word  *eq, *ne, *le, *ge, *minus, *True, *False, *temp, *and_, *or_;
-    
+    //Word  *eq, *ne, *le, *ge, *minus, *True, *False, *temp, *and_, *or_;
+    Tag *tag_ = new Tag();
+    Word *and_ = new Word("&&", tag_->AND);
+    Word *or_ = new Word("||", tag_->OR);
+    Word *eq = new Word("==", tag_->EQ);
+    Word *ne = new Word("!=", tag_->NE);
+    Word *le = new Word("<=", tag_->LE);
+    Word *ge = new Word(">=", tag_->GE);
+    Word *minus = new Word("minus", tag_->MINUS);
+    Word *True = new Word("true", tag_->TRUE);
+    Word *False = new Word("false", tag_->FALSE);
+    Word *temp = new Word("t", tag_->TEMP);
     Word():Token()
     {
-        Tag *tag_ = new Tag();
-        and_ = new Word("&&", tag_->AND);
-        or_ = new Word("||", tag_->OR);
-        eq = new Word("==", tag_->EQ);
-        ne = new Word("!=", tag_->NE);
-        le = new Word("<=", tag_->LE);
-        ge = new Word(">=", tag_->GE);
-        minus = new Word("minus", tag_->MINUS);
-        True = new Word("true", tag_->TRUE);
-        False = new Word("false", tag_->FALSE);
-        temp = new Word("t", tag_->TEMP);
     };
     Word(string s , int tag):Token(tag)
     {
         lexeme = s;
-
+        /*
         Tag *tag_ = new Tag();
         and_ = new Word("&&", tag_->AND);
         or_ = new Word("||", tag_->OR);
@@ -37,7 +36,7 @@ public:
         minus = new Word("minus", tag_->MINUS);
         True = new Word("true", tag_->TRUE);
         False = new Word("false", tag_->FALSE);
-        temp = new Word("t", tag_->TEMP);
+        temp = new Word("t", tag_->TEMP);*/
     };
     string toString()
     {
