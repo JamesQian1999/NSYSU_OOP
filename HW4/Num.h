3e6b2de8d540f489d49e615c004f05ed01336056
James <jamesqian1999@gmail.com>
2020-11-26 10:09:01 +0800

    core_dumped

--- HW4/Num.h
+++ HW4/Num.h
@@ -1,5 +1,25 @@
 #ifndef _NUM_H_
 #define _NUM_H_
-
+#include <sstream>
+#include "Token.h"
+#include "Tag.h"
+using namespace std;
+class Num : public Token
+{
+public:
+    int value;
+    Tag *tag = new Tag();
+    Num(int v):Token(tag->NUM)
+    {
+        value = v;
+    }
+    string toString()
+    {
+        ostringstream ss;
+        ss << value;
+        string s(ss.str());
+        return s;
+    }
+};
 
 #endif
--- HW4/Real.h
+++ HW4/Real.h
@@ -1,5 +1,24 @@
 #ifndef _REAL_H_
 #define _REAL_H_
-
+#include <sstream>
+#include "Token.h"
+using namespace std;
+class Real : public Token
+{
+public:
+    float value;
+    Tag *tag = new Tag();
+    Real(float v) : Token(tag->REAL)
+    {
+        value = v;
+    }
+    string toString()
+    {
+        ostringstream ss;
+        ss << value;
+        string s(ss.str());
+        return s;
+    }
+};
 
 #endif
